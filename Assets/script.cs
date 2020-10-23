0c24a0d5849acff55814d6a46d995224ba791ed1
PrakritiiJoshi <31702271+PrakritiiJoshi@users.noreply.github.com>
2020-10-23 04:28:01 -0400

    Add files via upload

---
+++ Assets/script.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(RawImage))]
+
+public class NewScript : MonoBehaviour
+{
+    [SerializeField]
+    private float waitToPlayforSchools = 1.0f;
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
